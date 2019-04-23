2152d431e709c61b8ac71032cf32b83fd1741574
ImpulsePie <31958880+ImpulsePie@users.noreply.github.com>
2019-04-23 11:02:58 +0000

    Add files via upload

---
+++ macos-virsh-amd.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+## Check if the script was executed as root
+[[ "$EUID" -ne 0 ]] && echo "Please run as root" && exit 1
+
+## Load the config file
+source "${BASH_SOURCE%/*}/configmacamd"
+
+## Check libvirtd
+[[ $(systemctl status libvirtd | grep running) ]] || systemctl start libvirtd && sleep 1 && LIBVIRTD=STOPPED
+
+## Memory lock limit
+[[ $ULIMIT != $ULIMIT_TARGET ]] && ulimit -l $ULIMIT_TARGET
+
+## Kill the Display Manager
+#systemctl stop sddm
+#sleep 1
+
+## Kill the console
+#echo 0 > /sys/class/vtconsole/vtcon0/bind
+#echo 0 > /sys/class/vtconsole/vtcon1/bind
+#echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+
+## Detach the GPU
+virsh nodedev-detach $VIRSH_USB > /dev/null 2>&1
+virsh nodedev-detach $VIRSH_SATA > /dev/null 2>&1
+#virsh nodedev-detach $VIRSH_GPU > /dev/null 2>&1
+#virsh nodedev-detach $VIRSH_GPU_AUDIO > /dev/null 2>&1
+virsh nodedev-detach $VIRSH_GPU2 > /dev/null 2>&1
+virsh nodedev-detach $VIRSH_GPU_AUDIO2 > /dev/null 2>&1
+
+
+## Load vfio
+modprobe vfio-pci
+
+      # -device ide-drive,bus=ide.0,drive=ISO \
+      # -drive id=ISO,if=none,snapshot=on,media=cdrom,file=$MACOS_ISO \
+      # -device nec-usb-xhci,id=xhci \
+      # -device vfio-pci,host=$IOMMU_USB \
+      #      -device vfio-pci,host=$IOMMU_SATA \
+      #      -usb -device usb-kbd -device usb-mouse \
+      #-device nec-usb-xhci,id=xhci \
+
+
+
+
+      
+MY_OPTIONS="+pcid,+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
+
+qemu-system-x86_64 -enable-kvm \
+      -nographic -vga none \
+      -m $MACOS_RAM \
+      -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,$MY_OPTIONS\
+      -machine pc-q35-2.11 \
+      -smp $MACOS_CORES,sockets=1,cores=$(( $MACOS_CORES / 2 )),threads=2 \
+      -device vfio-pci,host=$IOMMU_GPU2,multifunction=on,x-vga=on \
+      -device vfio-pci,host=$IOMMU_GPU_AUDIO2 \
+      -device vfio-pci,host=$IOMMU_USB \
+      -device vfio-pci,host=$IOMMU_SATA \
+      -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
+      -netdev user,id=net0 \
+      -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+      -drive if=pflash,format=raw,readonly,file=$MACOS_OVMF \
+      -drive if=pflash,format=raw,file=$MACOS_OVMF_VARS \
+	  -smbios type=2 \
+      -device ide-drive,bus=ide.2,drive=Clover \
+      -drive id=Clover,if=none,snapshot=on,format=qcow2,file=$MACOS_CLOVER \
+      -device ide-drive,bus=ide.1,drive=HDD \
+      -drive id=HDD,file=$MACOS_IMG,media=disk,format=qcow2,if=none >> $LOG 2>&1 &
+      #-object input-linux,id=kbd,evdev=/dev/input/by-id/usb-Corsair_Corsair_STRAFE_RGB_Gaming_Keyboard_0802E00BAEA90CA554E5D46FF5001942-event-kbd,grab_all=on,repeat=on \
+      #-object input-linux,id=kbd2,evdev=/dev/input/by-id/usb-Corsair_Corsair_STRAFE_RGB_Gaming_Keyboard_0802E00BAEA90CA554E5D46FF5001942-event-if00,grab_all=on,repeat=on \
+      #-object input-linux,id=mouse-event,evdev=/dev/input/by-id/usb-SteelSeries_SteelSeries_Rival_600-if01-event-mouse \
+      #-object input-linux,id=kbd3,evdev=/dev/input/by-id/usb-SteelSeries_SteelSeries_Rival_600-if02-event-kbd &
+
+
+
+## Wait for QEMU
+wait
+
+## Unload vfio
+modprobe -r vfio-pci
+modprobe -r vfio_iommu_type1
+modprobe -r vfio
+
+## Reattach the GPU
+#virsh nodedev-reattach $VIRSH_SATA > /dev/null 2>&1
+virsh nodedev-reattach $VIRSH_USB > /dev/null 2>&1
+virsh nodedev-reattach $VIRSH_GPU_AUDIO2 > /dev/null 2>&1
+virsh nodedev-reattach $VIRSH_GPU2 > /dev/null 2>&1
+#virsh nodedev-reattach $VIRSH_GPU_AUDIO > /dev/null 2>&1
+#virsh nodedev-reattach $VIRSH_GPU > /dev/null 2>&1
+
+## Reload the framebuffer and console
+echo 1 > /sys/class/vtconsole/vtcon0/bind
+nvidia-xconfig --query-gpu-info > /dev/null 2>&1
+echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
+
+## Reload the Display Manager
+#systemctl start sddm
+
+## If libvirtd was stopped then stop it
+[[ $LIBVIRTD == "STOPPED" ]] && systemctl stop libvirtd
+
+## Restore ulimit
+ulimit -l $ULIMIT
